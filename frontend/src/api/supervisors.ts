7c7874361507b033ab4a74d920622851c342aee1
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2026-01-03 16:30:56 +0400

    add supervisor apis

--- frontend/src/api/supervisors.ts
+++ frontend/src/api/supervisors.ts
@@ -4,3 +4,21 @@ export const getSupervisorProjects = async (supId) => {
     const { data } = await axios.get(`/supervisor/projects/${supId}`);
     return data;
 }
+export const getProjectExcels = async (prId) => {
+    const { data } = await axios.get(`/supervisor/excel/${prId}`);
+    return data;
+}
+export const getProjectAgents = async (prId) => {
+    const { data } = await axios.get(`/supervisor/project/${prId}`);
+    return data;
+}
+export const createExcel = async (_data) => {
+    const { data } = await axios.post(`/supervisor/excel`, _data);
+    return data;
+}
+
+export const updateExcel = async (excelId, _data) => {
+    const { data } = await axios.patch(`/supervisor/excel/${excelId}`, _data);
+    return data;
+}
+
