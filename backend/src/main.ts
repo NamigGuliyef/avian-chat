24e1f0979e10524271998086b52255a4808bc467
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2026-01-07 12:19:21 +0400

    fix issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
 
 
 
-  console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
-  await app.listen(process.env.PORT ?? 3000);
+  console.log(`Server is running on port ${process.env.PORT || 8004}`);
+  await app.listen(process.env.PORT! || 8004);
 }
 bootstrap();
